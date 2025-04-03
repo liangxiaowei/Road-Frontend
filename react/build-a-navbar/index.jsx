8ea5b186aa327240ca523ead2f1fa483977226b7
liangzhenwei <liangxiaowei18@sina.com>
2025-04-03 18:09:11 +0800

    note from freeecodecamp

---
+++ react/build-a-navbar/index.jsx
@@ -0,0 +1,13 @@
+export function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="nav-item">
+          <a href="#">Dashboard</a>
+        </li>
+        <li className="nav-item">2</li>
+        <li className="nav-item">3</li>
+      </ul>
+    </nav>
+  );
+}
