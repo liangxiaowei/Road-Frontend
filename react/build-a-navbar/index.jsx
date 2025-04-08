de8cda65f7cd51d507cadab17f83d762708776d1
pdway <liangxiaowei18@sina.com>
2025-04-09 07:50:53 +0800

    note from freecodecamp

--- react/build-a-navbar/index.jsx
+++ react/build-a-navbar/index.jsx
@@ -5,8 +5,25 @@ export function Navbar() {
         <li className="nav-item">
           <a href="#">Dashboard</a>
         </li>
-        <li className="nav-item">2</li>
-        <li className="nav-item">3</li>
+        <li className="nav-item">
+          <a href="#">Widgets</a>
+        </li>
+        <li className="nav-item">
+          {/* The aria-expanded attribute will communicate to screen reader users that the button triggers an expandable submenu. */}
+          <button aria-expanded="false">Apps</button>
+          {/* 下拉选择 */}
+          <ul className="sub-menu" aria-label="Apps">
+            <li>
+              <a href="#">Calendar</a>
+            </li>
+            <li>
+              <a href="#">Chat</a>
+            </li>
+            <li>
+              <a href="#">Email</a>
+            </li>
+          </ul>
+        </li>
       </ul>
     </nav>
   );
--- react/first-react-app/src/App.jsx
+++ react/first-react-app/src/App.jsx
@@ -1,28 +1,29 @@
-import { useState, Fragment } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, Fragment } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import Cat from "./Cat"; // 引入 Cat 组件
-
-
 
 // 自己定义的组件必须要大写，用来区分浏览器自带标签(小写)
 function Greeting() {
-  const name = "John"
+  const name = "John";
 
   /* 在这里 comment 不需要用括号包住 */
-  {/* The result will be Hello John*/}
-  // 这里不能用 class，因为 class 是 js 关键字，只能用 className 
+  {
+    /* The result will be Hello John*/
+  }
+  // 这里不能用 class，因为 class 是 js 关键字，只能用 className
   // name 用括号包住，可以使用 js 的东西，例如变量
   return <h1 className="title">Hello {name}</h1>;
 }
 
 function Greeting2() {
-  const name = "John"
+  const name = "John";
 
-  
   return (
-    <Fragment> { /*必须要有且只有一个父标签。也可以用 div */ }
+    <Fragment>
+      {" "}
+      {/*必须要有且只有一个父标签。也可以用 div */}
       <h1>Hello {name}</h1>
       <p>Nice to meet you.</p>
     </Fragment>
@@ -30,19 +31,61 @@ function Greeting2() {
 }
 
 function Greeting3() {
-  const name = "John"
+  const name = "John";
 
-  
   return (
-    <> { /* 相当于 Fragment 缩写 */ }
+    <>
+      {" "}
+      {/* 相当于 Fragment 缩写 */}
       <h1>Hello {name}</h1>
       <p>Nice to meet you.</p>
     </>
   );
 }
 
+function Greeting4(props) {
+  console.log(props);
+  // props.name = "1"; // props 是不可变的，不能重新赋值
+  return <h1>Hi {props.name}!</h1>;
+}
+
+function Greeting5({ name }) {
+  return <h1>Hi {name}</h1>;
+}
+
+function Greeting6({ isLoggedIn }) {
+  if (isLoggedIn) {
+    return <h1>Welcome back!</h1>;
+  }
+  return <h1>Please sign in</h1>;
+}
+
+function Greeting7({ isLoggedIn }) {
+  return <h1>{isLoggedIn ? "Welcome back!" : "Please sign in."}</h1>;
+}
+
+function Notification({ message }) {
+  return <div>{message && <p>{message}</p>}</div>;
+}
+
+// 解构对象并且赋值
+function DeveloperCard({ name, age, country }) {
+  return (
+    <div className="developer-card">
+      <h1>Developer: {name}</h1>
+      <p>Age: {age}</p>
+      <p>Country: {country}</p>
+    </div>
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const developerObj = {
+    name: "Alice",
+    age: 30,
+    country: "USA",
+  };
 
   return (
     <>
@@ -50,7 +93,16 @@ function App() {
         <Greeting /> {/* /> is required。(在这里 comment 一定要用括号包住) */}
         <Greeting2 />
         <Greeting3 />
-        <Cat />  {/* 使用导入的 Cat 组件 */}
+        <Greeting4 name="custom" />
+        <Greeting5 name="custom2" />
+        <Greeting6 isLoggedIn={true} />
+        <Greeting6 isLoggedIn={false} />
+        <Greeting7 isLoggedIn={true} />
+        <Notification />
+        <Notification message={"watch out"} />
+        {/* 解构对象 */}
+        <DeveloperCard {...developerObj} />
+        <Cat /> {/* 使用导入的 Cat 组件 */}
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -60,18 +112,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
